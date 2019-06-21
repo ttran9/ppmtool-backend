b82937af99baffb9353d0c7bdb2fa64dc44fd5de
jstran9 <wptran58@gmail.com>
2019-06-20 19:19:27 -0700

    adding front-end, react.

---
+++ src/main/resources/static/precache-manifest.218718322abe84e108f48ede182dd1da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f9728e7bff5574c84277d3234e5afb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff20a9444d80663bf93",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4827d69ef95c51966269",
+    "url": "/static/css/main.71a63cf9.chunk.css"
+  },
+  {
+    "revision": "fff20a9444d80663bf93",
+    "url": "/static/js/2.b4f1ce9c.chunk.js"
+  },
+  {
+    "revision": "4827d69ef95c51966269",
+    "url": "/static/js/main.8a3d437b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
