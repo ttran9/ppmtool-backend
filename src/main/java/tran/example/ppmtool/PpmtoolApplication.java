8ccb7489808b113d5438ee1fe05573727cd2f170
jstran9 <wptran58@gmail.com>
2019-06-23 01:38:54 -0700

    removing print statements.

--- src/main/java/tran/example/ppmtool/PpmtoolApplication.java
+++ src/main/java/tran/example/ppmtool/PpmtoolApplication.java
@@ -3,19 +3,10 @@ package tran.example.ppmtool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-
 @SpringBootApplication
 public class PpmtoolApplication {
 
     public static void main(String[] args) {
-        for(Map.Entry<Object, Object> entry: System.getProperties().entrySet()) {
-            System.out.println(entry.getKey() + " ==> " + entry.getValue());
-        }
-
-        for(Map.Entry<String, String> entry: System.getenv().entrySet()) {
-            System.out.println(entry.getKey() + " ==> " + entry.getValue());
-        }
         SpringApplication.run(PpmtoolApplication.class, args);
     }
 
