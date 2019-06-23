1c68879c9902be2ce235a67889975e585eb00ac0
jstran9 <wptran58@gmail.com>
2019-06-23 01:32:55 -0700

    printing all properties and env vars.

--- src/main/java/tran/example/ppmtool/PpmtoolApplication.java
+++ src/main/java/tran/example/ppmtool/PpmtoolApplication.java
@@ -3,12 +3,18 @@ package tran.example.ppmtool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class PpmtoolApplication {
 
     public static void main(String[] args) {
-        if(System.getProperty("JWT_PRIVATE_KEY") == null) {
-            System.out.println("we have a problem!!!!>!!");
+        for(Map.Entry<Object, Object> entry: System.getProperties().entrySet()) {
+            System.out.println(entry.getKey() + " ==> " + entry.getValue());
+        }
+
+        for(Map.Entry<String, String> entry: System.getenv().entrySet()) {
+            System.out.println(entry.getKey() + " ==> " + entry.getValue());
         }
         SpringApplication.run(PpmtoolApplication.class, args);
     }
