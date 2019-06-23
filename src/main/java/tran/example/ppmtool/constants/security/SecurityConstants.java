a9e18b2cc78d812a46bfd4c9753aab1e1071b8a7
jstran9 <wptran58@gmail.com>
2019-06-23 01:39:08 -0700

    fixing typo in env var.

--- src/main/java/tran/example/ppmtool/constants/security/SecurityConstants.java
+++ src/main/java/tran/example/ppmtool/constants/security/SecurityConstants.java
@@ -4,7 +4,7 @@ public class SecurityConstants {
 
     public static final String SIGN_UP_URLS = "/api/users/**";
     public static final String H2_URL = "/h2-console/**";
-    public static final String SECRET = System.getenv("JWT_PRIVATE_KEY"); // TODO: will refactor this with a more secure implementation
+    public static final String SECRET = System.getenv("JWT_PRIV_KEY "); // TODO: will refactor this with a more secure implementation
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final long EXPIRATION_TIME = 2700000; // in milliseconds so 2700 seconds / 45 mins.
