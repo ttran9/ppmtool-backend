b2ca5a97586b9ecab268b04acf6c1a1a87fe1fed
jstran9 <wptran58@gmail.com>
2019-06-28 17:44:56 -0700

    creating regexes to match urls.

---
+++ src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
@@ -0,0 +1,7 @@
+package tran.example.ppmtool.constants.mapping;
+
+public class MappingConstants {
+
+    public static final String URLS_REGEX = "^\\/[a-zA-Z]+$";
+    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[A-Z]{5}";
+}
