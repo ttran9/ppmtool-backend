d099ca957b639a4253036b867181a88ce4690033
jstran9 <wptran58@gmail.com>
2019-06-29 00:08:58 -0700

    modifying regex to allow for invalid project ids that are short in length.

--- src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
+++ src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
@@ -3,5 +3,5 @@ package tran.example.ppmtool.constants.mapping;
 public class MappingConstants {
 
     public static final String URLS_REGEX = "^\\/[a-zA-Z]+\\/{0,1}$";
-    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[a-zA-Z0-9]{4,5}";
+    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[a-zA-Z0-9]{1,5}";
 }
