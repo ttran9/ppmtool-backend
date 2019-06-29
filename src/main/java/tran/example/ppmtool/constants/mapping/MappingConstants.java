4d5f42170c7ddcd3f637c1d409b02ee47b8cd365
jstran9 <wptran58@gmail.com>
2019-06-28 22:09:48 -0700

    adding regexes to be used to match requests.

--- src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
+++ src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
@@ -3,5 +3,5 @@ package tran.example.ppmtool.constants.mapping;
 public class MappingConstants {
 
     public static final String URLS_REGEX = "^\\/[a-zA-Z]+$";
-    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[A-Z]{5}";
+    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[A-Z]{4,5}";
 }
