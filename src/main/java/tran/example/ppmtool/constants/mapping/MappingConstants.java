6e5a93913d220c4ee3593a9abed4eeaf0b32c681
jstran9 <wptran58@gmail.com>
2019-06-28 23:12:11 -0700

    accepting the trailing slash.

--- src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
+++ src/main/java/tran/example/ppmtool/constants/mapping/MappingConstants.java
@@ -2,6 +2,6 @@ package tran.example.ppmtool.constants.mapping;
 
 public class MappingConstants {
 
-    public static final String URLS_REGEX = "^\\/[a-zA-Z]+\\/*$";
+    public static final String URLS_REGEX = "^\\/[a-zA-Z]+\\/{0,1}$";
     public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[A-Z]{4,5}";
 }
